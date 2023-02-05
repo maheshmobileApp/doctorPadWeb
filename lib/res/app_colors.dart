cc73781934f11affcdd3b32f16f05c737df9bb39
suresh <115678657+sureshchimaladinne@users.noreply.github.com>
2023-02-05 20:01:49 +0530

    add loginwebview

--- lib/res/app_colors.dart
+++ lib/res/app_colors.dart
@@ -4,4 +4,5 @@ class AppColors {
   const AppColors._();
   static const Color nameTextColor = Color(0xff3367F7);
   static const Color app_bg_color = Color.fromRGBO(122, 255, 12, 0.5);
+
 }
--- lib/view/login/login_web_view.dart
+++ lib/view/login/login_web_view.dart
@@ -10,103 +10,127 @@ class LoginWebView extends StatelessWidget {
   bool checkedValue = false;
   bool checkboxValue = false;
   bool isAlredyUser = false;
-
-  bool isEmailValid(String email) {
-    bool emailValid = RegExp(r'^.+@[a-zA-Z]+\.{1}[a-zA-Z]+(\.{0,1}[a-zA-Z]+)$')
-        .hasMatch(email);
-    return emailValid;
+  RegExp pass_valid = RegExp(r"(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W)");
+  //A function that validate user entered password
+  bool validatePassword(String pass){
+    String _password = pass.trim();
+    if(pass_valid.hasMatch(_password)){
+      return true;
+    }else{
+      return false;
+    }
   }
+
+  bool isValidPhoneNumber(String? phone) =>
+      RegExp(r'(^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$)')
+          .hasMatch(phone ?? '');
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Center(
-        child: Form(
-          key: _formKey,
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      body: Center(
+        child: SingleChildScrollView(
+        child:Container(
+          height: 400,
+          width: 450,
+          color: Colors.white,
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Card(
-                  color: Colors.blue,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Image.asset("assets/images/1.png"),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      TextFormField(
-                        decoration: InputDecoration(
-                            prefixIcon: Icon(Icons.email),
-                            labelText: 'Email',
-                            hintText: "Please enter email"),
-                        keyboardType: TextInputType.text,
-                        validator: (email) {
-                          if (isEmailValid(email ?? ""))
-                            return null;
-                          else
-                            return 'Enter a valid email address';
-                        },
-                      ),
-                      SizedBox(
-                        height: 30,
-                      ),
-                      TextFormField(
-                        decoration: InputDecoration(
-                            prefixIcon: Icon(Icons.password_outlined),
-                            labelText: 'Password',
-                            hintText: "Please enter password"),
-                        keyboardType: TextInputType.emailAddress,
-                        validator: (value) {
-                          if (value!.isEmpty) {
-                            return 'Enter a valid password';
-                          }
-                          return null;
-                        },
-                      ),
-                      SizedBox(
-                        height: 30,
-                      ),
-                      ElevatedButton(
-                        onPressed: () {
-                          if (_formKey.currentState!.validate()) {
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              const SnackBar(content: Text('Processing Data')),
-                            );
+              Image.asset(
+                'assets/doctpad.png',
+                height: 100,
+                width: 100,
+              ),
+              SizedBox(
+                height: 23,
+              ),
+              Form(
+                key: _formKey,
+                child: TextFormField(
+                  decoration: InputDecoration(
+                      labelText: 'User Mobile Number',
+                      prefixIcon: Icon(
+                        Icons.person,
+                        color: Colors.blue,
+                      )),
+                  keyboardType: TextInputType.number,
+                  onChanged: (value) {
+                    _formKey.currentState?.validate();
+                  },
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "Please Enter a Phone Number";
+                    } else if (!RegExp(
+                            r'^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$')
+                        .hasMatch(value)) {
+                      return "Please Enter a Valid Phone Number";
+                    }
+                  },
+                ),
+              ),
+              SizedBox(
+                height: 23,
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                    labelText: 'Password',
+                    prefixIconColor: Colors.blue,
+                    prefixIcon: Icon(Icons.lock)),
+                validator: (value){
+                  if(value!.isEmpty){
+                    return "Please enter password";
+                  }else{
+                    //call function to check password
+                    bool result = validatePassword(value);
+                    if(result){
+                      // create account event
+                      return null;
+                    }else{
+                      return " Password should contain Capital, small letter & Number & Special";
+                    }
+                  }
+                },
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text('Processing Data')),
+                    );
 
-                            if (isAlredyUser) {
-                            } else {
-                              isAlredyUser = true;
-                            }
-                          }
-                        },
-                        child: Text(
-                          'Submit',
-                        ),
-                        style: ElevatedButton.styleFrom(
-                          shape: StadiumBorder(),
-                          primary: Colors.black,
-                          padding: EdgeInsets.symmetric(
-                              horizontal: 80, vertical: 10),
-                          textStyle: TextStyle(
-                            fontSize: 20,
-                          ),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 50,
-                      ),
-                      RichText(
-                          text: TextSpan(
-                        text: 'forget password',
-                      ))
-                    ],
-                  )),
+                    if (isAlredyUser) {
+                    } else {
+                      isAlredyUser = true;
+                    }
+                  }
+                },
+                child: Text(
+                  'Login',
+                ),
+                style: ElevatedButton.styleFrom(
+                  shape: StadiumBorder(),
+                  primary: Colors.blue,
+                  padding: EdgeInsets.symmetric(horizontal: 80, vertical: 10),
+                  textStyle: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 35,
+              ),
+              RichText(
+                  text: TextSpan(
+                      text: 'Forget Password?',
+                      style: TextStyle(color: Colors.blue)))
             ],
           ),
         ),
+      ),
       ),
     );
   }
