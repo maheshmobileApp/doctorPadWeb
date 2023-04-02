ce1c83c8b3dcce7e75c641b650c272c63f05a621
suresh <sureshchimaladinne9063@gmail.com>
2023-04-02 17:55:15 +0530

    names changes

--- lib/res/constants/go_router.dart
+++ lib/res/constants/go_router.dart
@@ -1,6 +1,6 @@
 import 'package:cgg_base_project/res/constants/routes_constants.dart';
 import 'package:cgg_base_project/view/dash_board.dart';
-import 'package:cgg_base_project/view/hospital_web_view/hospital_web_view.dart';
+import 'package:cgg_base_project/view/hospital_view/hospital_view.dart';
 import 'package:cgg_base_project/view/login/login_web_view.dart';
 import 'package:cgg_base_project/view/login_view.dart';
 import 'package:flutter/material.dart';
@@ -8,10 +8,10 @@ import 'package:go_router/go_router.dart';
 import '../../view/add_doctor_successfully/add_doctor_successfully.dart';
 import '../../view/add_hospital_successfully/add_hospital_successfully.dart';
 import '../../view/doctor_details/doctor_details.dart';
-import '../../view/doctor_web_details/doctor_web_details.dart';
-import '../../view/doctor_web_view/doctor_web_view.dart';
-import '../../view/hospital_details/hospital_details.dart';
-import '../../view/hospital_web_details/hospital_web_details.dart';
+import '../../view/add_doctor/add_doctor.dart';
+import '../../view/doctor_view/doctor_view.dart';
+import '../../view/add_hospital/add_hospital.dart';
+import '../../view/hospital__details/hospital_details.dart';
 
 final GoRouter routes = GoRouter(
   initialLocation: RoutesList.loginWebView,
